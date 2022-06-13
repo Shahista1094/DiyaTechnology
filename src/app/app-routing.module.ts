6aba2924ffc55989b30c56a2455acedb1dc7d90a
AshishTroology <ashish.maurya@troology.com>
2022-06-13 12:30:38 +0530

    changes morning

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrintDealComponent } from './print-deal/print-deal.component';
+import { PrintTableComponent } from './print-table/print-table.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+    { path: 'view-quote/:id', component: PrintTableComponent },
+    { path: 'view-deal/:id', component: PrintDealComponent },
+  ]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
---
+++ src/app/print-deal/print-deal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-print-deal',
+  templateUrl: './print-deal.component.html',
+  styleUrls: ['./print-deal.component.css']
+})
+export class PrintDealComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
